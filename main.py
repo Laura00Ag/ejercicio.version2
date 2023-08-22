dfeb83bb78302538ec573f393d206380264424b6
Laura00Ag <laura.acevedog@udea.edu.co>
2023-08-22 09:26:47 -0500

    Adicion de la funcion y condicional al menu

--- main.py
+++ main.py
@@ -72,8 +72,12 @@ mi_sistema = Sistema()
 while True:
     opcion = int(input("1. Nuevo paciente\n - 2. Numero de paciente\n - 3. Datos paciente\n - 4. Salir:  \n"))
     if opcion == 1:
-    
-        mi_sistema.ingresarPaciente()
+        if mi_sistema.VerificarPaciente() == False:
+            print("El paciente ya se encuentra registrado.")
+            continue
+        else:    
+            mi_sistema.ingresarPaciente()
+            
     elif opcion == 2:
         print("Ahora hay: " + str(mi_sistema.verNumeroPacientes()))
     elif opcion == 3:
